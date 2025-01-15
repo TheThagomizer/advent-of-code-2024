37961823cdf9af03ff4f71011a5d57de437db7f8
Mark Christiansen <mcweatherlight@gmail.com>
2025-01-15 13:26:09 -0700

    Day 3 first passing iteration

--- day-03/pt2.rb
+++ day-03/pt2.rb
@@ -1,7 +1,23 @@
-total = File.readlines('test-input')
-    .map(&:strip)
-      .sum { |line|
-        line.scan(/mul\((\d{1,3}),(\d{1,3})\)/).sum { |a, b| a.to_i * b.to_i }
-      }
-      
-puts total.inspect
+state = true
+
+total = File.readlines('input')
+  .map(&:strip)
+  .sum do |line|
+    line.scan(/mul\((\d{1,3}),(\d{1,3})\)|(don't\(\)|do\(\))/).sum { |a, b, toggle|
+      if toggle != nil
+        if toggle == "do()"
+          state = true
+        elsif toggle == "don't()"
+          state = false
+        end
+        next 0
+      end
+      if state == true
+        a.to_i * b.to_i
+      elsif state == false
+        0
+      end
+    }
+  end
+
+puts total
