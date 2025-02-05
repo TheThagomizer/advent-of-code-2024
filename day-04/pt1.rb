429e052733f72651e5ceca60b94c1c0a76129678
Mark Christiansen <mcweatherlight@gmail.com>
2025-02-04 21:38:44 -0700

    Day 4 First Working Draft

--- day-04/pt1.rb
+++ day-04/pt1.rb
@@ -1,4 +1,22 @@
 require "matrix"
 
-grid = File.readlines("test-input").map(&:strip).map(&:chars)
+grid = File.readlines("input").map(&:strip).map(&:chars)
 matrix = Matrix[*grid]
+total_valid_paths = 0
+
+TARGET = ["X", "M", "A", "S"].freeze
+MAX_J = matrix.row_count.freeze
+
+dirs = ([-1, 0, 1].repeated_permutation(2).to_a - [[0,0]]).map { |dx, dy| Vector[dx, dy] }
+matrix.each_with_index { |char, ri, ci| 
+  next unless char == "X"
+      pos = Vector[ri, ci]
+      valid_paths = dirs.count { |dv|
+        (1..3).all? { |i|
+        next_pos = pos + (dv * i)
+        next_pos.all? { |j| j >= 0 && j < MAX_J } && matrix[*next_pos] == TARGET[i]
+        }
+      }
+      total_valid_paths += valid_paths
+    }
+puts total_valid_paths
