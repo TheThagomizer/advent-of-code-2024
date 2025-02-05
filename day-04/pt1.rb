1354c0bc4210b9308154680cdb357ac39b82f711
Mark Christiansen <mcweatherlight@gmail.com>
2025-02-04 23:35:43 -0700

    Cleanup

--- day-04/pt1.rb
+++ day-04/pt1.rb
@@ -2,14 +2,13 @@ require "matrix"
 
 grid = File.readlines("input").map(&:strip).map(&:chars)
 matrix = Matrix[*grid]
-total_valid_paths = 0
 
 TARGET = ["X", "M", "A", "S"].freeze
 MAX_J = matrix.row_count.freeze
 
 dirs = ([-1, 0, 1].repeated_permutation(2).to_a - [[0,0]]).map { |dx, dy| Vector[dx, dy] }
-matrix.each_with_index { |char, ri, ci| 
-  next unless char == "X"
+total = matrix.each_with_index.sum { |char, ri, ci| 
+  next 0 unless char == "X"
       pos = Vector[ri, ci]
       valid_paths = dirs.count { |dv|
         (1..3).all? { |i|
@@ -17,6 +16,5 @@ matrix.each_with_index { |char, ri, ci|
         next_pos.all? { |j| j >= 0 && j < MAX_J } && matrix[*next_pos] == TARGET[i]
         }
       }
-      total_valid_paths += valid_paths
-    }
-puts total_valid_paths
+}
+puts total
