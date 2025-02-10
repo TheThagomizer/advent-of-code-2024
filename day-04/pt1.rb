1e7acd71ad7f85e454172273d174f2ed0a27336e
Mark Christiansen <mcweatherlight@gmail.com>
2025-02-10 16:49:35 -0700

    Update to finalize Day 04

--- day-04/pt1.rb
+++ day-04/pt1.rb
@@ -9,12 +9,13 @@ MAX_J = matrix.row_count.freeze
 dirs = ([-1, 0, 1].repeated_permutation(2).to_a - [[0,0]]).map { |dx, dy| Vector[dx, dy] }
 total = matrix.each_with_index.sum { |char, ri, ci| 
   next 0 unless char == "X"
-      pos = Vector[ri, ci]
-      valid_paths = dirs.count { |dv|
-        (1..3).all? { |i|
+    pos = Vector[ri, ci]
+    valid_paths = dirs.count { |dv|
+      (1..3).all? { |i|
         next_pos = pos + (dv * i)
-        next_pos.all? { |j| j >= 0 && j < MAX_J } && matrix[*next_pos] == TARGET[i]
-        }
+        next_pos.all? { |j| 
+          j >= 0 && j < MAX_J } && matrix[*next_pos] == TARGET[i]
       }
+    }
 }
 puts total
---
+++ day-04/pt2.rb
@@ -0,0 +1,34 @@
+require "matrix"
+require "pry-byebug"
+require "pry-rescue"
+require "pry-stack_explorer"
+
+grid = File.readlines("input").map(&:strip).map(&:chars)
+matrix = Matrix[*grid]
+
+MAX_SIZE = matrix.row_count.freeze
+TARGET = ["M", "A", "S"].freeze
+
+dirs = [
+  Vector[-1, 1],
+  Vector[1, 1],
+  Vector[-1, -1],
+  Vector[1, -1],
+]
+
+total = matrix.each_with_index.count {|char, ri, ci|
+  next unless char == "A"
+  pos = Vector[ri, ci]
+  dirs.count {|dv|
+    (-1..1).all? { |i|
+    #binding.pry
+      next_pos = pos + (dv * i)
+      next_pos.all? { |j| 
+      #binding.pry
+        j >=0 && j < MAX_SIZE
+      } && matrix[*next_pos] == TARGET[i+1]
+    }  
+  } == 2
+}
+
+puts total.inspect
